78594f9a7115f0a92dd0fbb13316947393b675f1
anika <anika@ntl-HP-Pavilion-Laptop-15-cc1xx>
2022-02-06 18:21:32 +0600

    last

---
+++ external1.js
@@ -0,0 +1,54 @@
+// document.write("Anika Tabassum <br>");
+// document.write("Subject: CSE <br>");
+// document.write("Language: Java <br>");
+
+// var alphabet = prompt("Enter digit");
+
+// alphabet = alphabet.toLowerCase();
+
+// switch(alphabet){
+//  case "A":
+//  console.log("Vowel");
+//  break;
+//  case "E":
+//  console.log("Vowel");
+//  break;
+//  case "I":
+//  console.log("Vowel");
+//  break;
+//  case "O":
+//  console.log("Vowel");
+//  break;
+//  case "U":
+//  console.log("Vowel");
+//  break;
+//  default:
+//  console.log("Consonant");
+// }
+//---------------------
+
+// function square(){
+// 	var number = 5;
+// 	var result = number*number;
+// 	document.write(result);
+// }
+
+// square();
+//-------------------
+// var student1={
+
+// 	name: "Anika",
+// 	age: 24,
+// 	Subject: "CSE",
+// 	lang:["bangla","english"],
+// }
+// console.log(student1.name);
+//---------------
+// var currentDate = date.getDate();
+// console.log(currentDate)
+//----------------
+
+var myheading = document.getElementById("heading 1");
+//myheading.innerHTML = "Hello";
+
+document.querySelector("p").innerHTML= "This is changed";
---
+++ index.js
@@ -0,0 +1,19 @@
+// console.log("Anika Tabassum");
+// var life= 100;
+
+// life = life-20;
+// console.log(life);
+
+// function logger(){
+// 	console.log("party time");
+// 	console.log("party time");
+// 	console.log("party time");
+// 	console.log("party time");
+// }
+
+// logger();
+
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
+document.getElementById("demo").innerHTML = myFunction(4, 3);
